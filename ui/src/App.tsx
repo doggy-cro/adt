753d39061e3f331d768a2b27dc3f455ab071f6bf
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2022-04-18 15:13:58 +0200

    init ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/app.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='brand'>
+        <p className='adt'>ADT</p>
+        <p className='tracker'>tracker</p>
+        <div className='line'></div>
+      </div>
+      <div className='board'>
+      </div>
+    </div>
+  );
+}
+
+export default App;
