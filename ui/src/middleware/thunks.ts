16154e2d4b3a4b344e22eb0099666bb7905d909a
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2022-04-21 09:38:17 +0200

    add redux-thunk

---
+++ ui/src/middleware/thunks.ts
@@ -0,0 +1,7 @@
+import { saveChainData } from '../api';
+import { ADD_CHAIN_DATA } from '../actions/chainActions';
+
+// Thunk function
+export function sendChainDataToServer(record: any) {
+  return async function sendThunk(dispatch: any, getState: any) {};
+}
--- ui/src/store/store.js
+++ ui/src/store/store.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from '../reducers/rootReducer';
-import { callToApiMiddleware } from '../addons/middleware';
 
-const composedEnhancer = composeWithDevTools(
-  applyMiddleware(callToApiMiddleware)
-);
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
 const store = createStore(rootReducer, composedEnhancer);
 
